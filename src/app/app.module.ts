27bab308001caa7c8b97a433b811131d0e487c7b
Mayura Ramanayaka <mayura.ramanayake@gmail.com>
2019-09-21 01:16:46 +0530

    Added the add order page
    - Used angular reactive forms to handle the dynamic behavior
    - Created different methods to add different items in the form
    - Added basic styling for the form elements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { AppConfig } from '../environments/environment.dev';
+import { ReactiveFormsModule } from '@angular/forms';
 
 // NG translate
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
@@ -48,6 +49,7 @@ export function HttpLoaderFactory(http: HttpClient) {
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
+    ReactiveFormsModule,
     CoreModule,
     TranslateModule.forRoot({
       loader: {
--- src/app/components/orders/orders-create/orders-create.component.ts
+++ src/app/components/orders/orders-create/orders-create.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UIService } from '../../../core/services';
+import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-orders-create',
@@ -8,11 +9,101 @@ import { UIService } from '../../../core/services';
 })
 export class OrdersCreateComponent implements OnInit {
   currentDate;
+  ordersForm: FormGroup;
 
-  constructor(public ui: UIService) { }
+  constructor(public ui: UIService, private fb: FormBuilder) { }
 
   ngOnInit() {
     this.currentDate = new Date();
+    this.ordersForm = this.fb.group({
+      customerName: '',
+      contactNumber: '',
+      description: '',
+      amount: this.fb.group({
+        totalAmount: 0,
+        prepaidAmount: 0
+      }),
+      items: this.fb.array([]),
+      done: false
+    });
+
+    // temp
+    this.ordersForm.valueChanges.subscribe(value => console.log(value));
   }
 
+  // get items form array
+  get itemsForms() {
+    return this.ordersForm.get('items') as FormArray;
+  }
+
+  // add pants template
+  addPantsItemTemplate() {
+    const tmplt = this.fb.group({
+      type: 'PANT',
+      kneeOne: '',
+      kneeTwo: '',
+      height: '',
+      waist: '',
+      sheet: '',
+      crotch: '',
+      fleet: '',
+      heel: '',
+      pocket: '',
+      backPocket: ''
+    });
+
+    this.itemsForms.push(tmplt);
+  }
+
+  // add shirts template
+  addShirtsItemTemplate() {
+    const tmplt = this.fb.group({
+      type: 'SHIRT',
+      height: '',
+      york: '',
+      sleeveLength: '',
+      sleeveAround: '',
+      chest: '',
+      waist: '',
+      collar: '',
+      addOn: ''
+    });
+
+    this.itemsForms.push(tmplt);
+  }
+
+  // add coats template
+  addCoatsItemTemplate() {
+    const tmplt = this.fb.group({
+      type: 'COAT',
+      heightFront: '',
+      heightBack: '',
+      york: '',
+      goldMiddle: '',
+      sleeveLength: '',
+      sleeveEnd: '',
+      sleeveMiddle: '',
+      collar: ''
+    });
+
+    this.itemsForms.push(tmplt);
+  }
+
+  // add west coats template
+  addWestCoatsItemTemplate() {
+    const tmplt = this.fb.group({
+      type: 'WESTCOAT',
+      height: '',
+      york: '',
+      chest: '',
+      waist: ''
+    });
+
+    this.itemsForms.push(tmplt);
+  }
+
+  // remove a template based on index
+  removeItemTemplate(i) {
+    this.itemsForms.removeAt(i);
+  }
 }
