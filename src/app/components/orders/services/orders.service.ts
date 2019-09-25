6764782ca13558cb6f5905d13b10cd58d4f68017
Mayura Ramanayaka <mayura.ramanayake@gmail.com>
2019-09-26 01:53:01 +0530

    Added the missing createdAt property to order documents

--- src/app/components/orders/services/orders.service.ts
+++ src/app/components/orders/services/orders.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import * as firebase from 'firebase/app';
 
 @Injectable({
   providedIn: 'root'
@@ -13,8 +14,10 @@ export class OrdersService {
 
   // add a new record to the orders collection
   async createNewOrder(order) {
+    const createdAt = firebase.firestore.FieldValue.serverTimestamp();
+
     try {
-      await this.ordersCollection.add(order);
+      await this.ordersCollection.add({ ...order, createdAt });
     } catch (e) {
       console.error("[ERRRROR]", e);
     }
